d7a1faf550de8ee7205524c3735cbaeae93e486c
Åsmund Grammeltvedt <asmundg@big-oil.org>
2015-08-10 17:36:14 +0200

    Fix timezone handling
    Take the opportunity to use arrow for all datetime handling.

--- btcal/btcal.py
+++ btcal/btcal.py
@@ -1,15 +1,14 @@
 from datetime import timedelta
 import re
 
+import arrow
 from bs4 import BeautifulSoup
-from dateutil import parser as dtparser
 from icalendar import Calendar, Event, vText
-import pytz
 import requests
 
 
 def scrape(url):
-    return BeautifulSoup((requests.get(url).text))
+    return BeautifulSoup((requests.get(url).text), 'html.parser')
 
 
 class BTScraper(object):
@@ -29,31 +28,39 @@ class BTScraper(object):
         description = ' '.join(
             (event.select('.activity_trip_view_ingress')[0].get_text(),
              event.select('.activity_trip_view_content')[0].get_text()))
-        time = self.find_time(description)
+        time = event_time(description)
         return [
             link.string,
             description,
             link.get('href'),
-            (dtparser.parse(
-                block.select(
-                    '.activity_list_loc_dates b')[0].string.strip() + time,
-                dayfirst=True)
-             .replace(tzinfo=pytz.timezone('Europe/Oslo'))
-             .astimezone(pytz.utc))]
+            (event_date(block)
+             .replace(hour=time.hour, minute=time.minute, tzinfo=time.tzinfo)
+             .to('UTC'))]
 
     def absolutify(self, url):
         return self.root + url
 
-    def find_time(self, description):
+
+def event_time(event_details):
+    time = re.findall(
+        'kl\.? ([0-9]{2}[.:]?[0-9]{2})', event_details, re.MULTILINE)
+    if not time:
         time = re.findall(
-            'kl\.? ([0-9]{2}[.:]?[0-9]{2})', description, re.MULTILINE)
-        if not time:
-            time = re.findall(
-                '([0-9]{2}[.:]?[0-9]{2})', description, re.MULTILINE)
-        if not time:
-            time = ['00:00']
-        time = time[0].replace('.', '').replace(':', '')
-        return time
+            '[^0-9]([0-9]{2}[.:]?[0-9]{2})[^0-9]', event_details,
+            re.MULTILINE)
+    if not time:
+        time = ['00:00']
+    time = time[0].replace('.', '').replace(':', '')
+    return arrow.get(time, 'HHmm').replace(tzinfo='Europe/Oslo')
+
+
+def event_date(event_summary):
+    return arrow.get(
+        ''.join(re.findall(
+            '[0-9]{2}\.[0-9]{2}\.[0-9]{4}',
+            event_summary.select(
+                '.activity_list_loc_dates b')[0].string.strip())),
+        'DD.MM.YYYY')
 
 
 def icalize(events):
@@ -65,16 +72,17 @@ def icalize(events):
         ev.add('summary', vText(header))
         ev.add('description', vText(description))
         ev.add('location', url)
-        ev.add('dtstart', date)
-        ev.add('dtend', date + timedelta(seconds=60 * 60))
-        ev.add('dtstamp', date)
+        ev.add('dtstart', date.datetime)
+        ev.add('dtend', date.replace(seconds=60 * 60).datetime)
+        ev.add('dtstamp', date.datetime)
         cal.add_component(ev)
     return cal.to_ical()
 
 
-scraper = BTScraper()
-print(
-    icalize(
-        [header, description, scraper.absolutify(url), date]
-        for header, description, url, date in scraper.get_events(
-            scraper.get_overview())))
+def main():
+    scraper = BTScraper()
+    print(
+        icalize(
+            [header, description, scraper.absolutify(url), date]
+            for header, description, url, date in scraper.get_events(
+                scraper.get_overview())))
